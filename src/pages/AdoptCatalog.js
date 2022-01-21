01c2e58d287cc98f916b351d40a9adf84bbecba6
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-01-21 14:19:47 +0800

    upload image update

--- src/pages/AdoptCatalog.js
+++ src/pages/AdoptCatalog.js
@@ -1,4 +1,3 @@
-import list from "./catalog.json";
 import categories from "./categories.json";
 import { useEffect, useState } from "react";
 import NavbarLayout from "../components/Navbar";
@@ -9,10 +8,10 @@ const AdoptCatalog = () => {
     const [adoptData, setAdoptData] = useState([]);
     const [animalCategories, setAnimalCategories] = useState([]);
     const [currentCategory, setCurrentCategory] = useState('');
-    const title = "test";
+    const [keyword, setKeyword] = useState('');
 
     useEffect(() => {
-        fetch(`http://localhost:8000/adopt?title=${title}&category=${currentCategory}`, 
+        fetch(`http://localhost:8000/adopt?title=${keyword}&category=${currentCategory}`, 
         {
             method: "GET",
             headers: {
@@ -22,32 +21,44 @@ const AdoptCatalog = () => {
         .then(res => res.json())
         .then(result => {
             // setAdoptData(result)
-            console.log(result);
-        });
+            console.log(result.data);
+            setAdoptData(result.data)
+        }, []);
 
-        setAdoptData(list);
         setAnimalCategories(categories);
-    }, [currentCategory])
+    }, [currentCategory, keyword])
+
+    const handleCategoryClick = (categoryId) => {
+        if (currentCategory === categoryId) {
+            setCurrentCategory('');
+        } else {
+            setCurrentCategory(categoryId);
+        }
+    }
 
     return (
         <> 
             <NavbarLayout />
             <div className="bg-orange-50 h-max min-h-screen">
-                <h1 className="text-center py-7 md:py-7 text-2xl font-bold">Adoption</h1>
+                <h1 className="text-center py-3 text-2xl font-bold">Adoption</h1>
+                <div className="flex justify-center my-3 gap-1 mb-7">
+                    <input type="search" className="w-3/4 md:w-2/4 border-2" onChange={(e) => setKeyword(e.target.value)} />
+                    <button className="py-2 px-3 bg-slate-200 rounded-md">Search</button>
+                </div>
                 <div className="flex justify-between mx-3 md:mx-12 mb-7 md:mb-7">
                     {animalCategories.map(item => (
-                        <button key={item.id} onClick={() => setCurrentCategory(item.id)} className={`w-16 md:w-20 font-bold bg-slate-500 text-center py-2 rounded-md ${currentCategory === item.id ? 'bg-orange-500' : 'bg-slate-500'}`}>{item.name}</button>
+                        <button key={item.id} onClick={() => handleCategoryClick(item.id)} className={`w-16 md:w-20 font-bold bg-slate-500 text-center py-2 rounded-md ${currentCategory === item.id ? 'bg-orange-500' : 'bg-slate-500'}`}>{item.name}</button>
                     ))}
                 </div>
                 <div className="grid grid-cols-2 md:grid-cols-5 mx-3 pb-3 md:mx-12 gap-5">
                     {adoptData.map((item, i) => (
                         <div key={i} className="text-lg border-2 py-3 px-2 bg-slate-200 rounded-md">
-                            <img className="h-[75%]" src={`${item.image}`} alt="item-preview" />
-                            <div className="flex gap-3 justify-between">
+                            <img className="h-[62%]" src={`${item.img}`} alt="item-preview" />
+                            <div>
                                 <h1 className="font-bold">{item.name}</h1>
-                                <h1 className="font-medium mt-1 text-base">{3} Bulan</h1>
+                                <h1 className="font-medium mt-1 text-base">{item.age} Bulan</h1>
+                                <h1 className="font-semibold">Species: <span className="font-bold">{item.animal_race}</span></h1>
                             </div>
-                            <h3 className="font-semibold text-center">{item.race}</h3>
                         </div>
                     ))}
                 </div>
