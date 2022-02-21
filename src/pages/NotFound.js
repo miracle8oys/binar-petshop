a2a6f2e6e48e6effaa3bd11d8d74544d06aac296
Kusuma Sandi <wijayakusumasandi@gmail.com>
2022-02-21 23:10:27 +0800

    add not found handler

---
+++ src/pages/NotFound.js
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return ( 
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div>
+                <h1 className="text-7xl text-center">404</h1>
+                <p className="text-5xl">Page Not Found</p>
+            </div>
+        </div>
+     );
+}
+ 
+export default NotFound;
